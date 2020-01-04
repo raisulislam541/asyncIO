55e7b3a46b8799cb0d7a804fed42c76273e34168
Sajal <raisulislam541@gmail.com>
2020-01-05 00:39:41 +0600

    added: asyncIO for a soapapi request

---
+++ async.py
@@ -0,0 +1,19 @@
+import aiohttp
+import asyncio
+
+
+# for soap api request
+async def main():
+    async with aiohttp.Clientsession() as session:
+        data = '''
+        xml data
+        '''
+        headers = {}
+        url = "url"
+        # for post request
+        async with session.post(url=url, ssl=False, headers=headers,
+                                data=data) as resp:
+            responded_data = await resp.json()
+
+
+asyncio.run(main())
